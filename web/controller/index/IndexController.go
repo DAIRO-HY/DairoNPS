9e568549cc634dc3eec92cb13f546e869bb9c1ed
zhoulq <1064348912@qq.com>
2024-10-29 19:48:59 +0900

    删除无用的统计信息

--- web/controller/index/IndexController.go
+++ web/controller/index/IndexController.go
@@ -69,22 +69,22 @@ func getData() form.IndexOutForm {
 	var memStats runtime.MemStats
 	runtime.ReadMemStats(&memStats)
 	outForm := form.IndexOutForm{
-		OnlineClientCount:  nps_client.OnlineCount(),              //在线客户端数量
-		TcpBridgeCount:     nps_bridge.GetBridgeCount(),           //当前TCP会话数
-		TcpPoolCount:       nps_pool.GetPoolCount(),               //当前TCP连接池
-		UdpSessionCount:    0,                                     //当前UDP会话数
-		UdpPoolCount:       0,                                     //当前UDP连接池
-		InDataTotal:        Number.ToDataSize(systemDto.InData),   //入网流量
-		OutDataTotal:       Number.ToDataSize(systemDto.OutData),  //出网流量
-		ProxyCount:         nps_channel_proxy.GetProxyCount(),     //当前正在代理数
-		ForwardCount:       forward.GetAcceptCount(),              //端口转发代理端口数量
-		ForwardBridgeCount: forward.GetBridgeCount(),              //端口转发当前桥接数量
-		NumGoroutine:       runtime.NumGoroutine(),                //当前协程数
-		Memory:             Number.ToDataSize(memStats.Alloc),     //内存分配
-		SystemMemory:       Number.ToDataSize(memStats.Sys),       //系统内存占用
-		HeapAlloc:          Number.ToDataSize(memStats.HeapAlloc), //堆内存分配
-		HeapSys:            Number.ToDataSize(memStats.HeapSys),   //堆内存系统占用
-		NumGC:              memStats.NumGC,                        //垃圾回收次数
+		OnlineClientCount:  nps_client.OnlineCount(),             //在线客户端数量
+		TcpBridgeCount:     nps_bridge.GetBridgeCount(),          //当前TCP会话数
+		TcpPoolCount:       nps_pool.GetPoolCount(),              //当前TCP连接池
+		UdpSessionCount:    0,                                    //当前UDP会话数
+		UdpPoolCount:       0,                                    //当前UDP连接池
+		InDataTotal:        Number.ToDataSize(systemDto.InData),  //入网流量
+		OutDataTotal:       Number.ToDataSize(systemDto.OutData), //出网流量
+		ProxyCount:         nps_channel_proxy.GetProxyCount(),    //当前正在代理数
+		ForwardCount:       forward.GetAcceptCount(),             //端口转发代理端口数量
+		ForwardBridgeCount: forward.GetBridgeCount(),             //端口转发当前桥接数量
+		NumGoroutine:       runtime.NumGoroutine(),               //当前协程数
+		Memory:             Number.ToDataSize(memStats.Alloc),    //内存分配
+		//SystemMemory:       Number.ToDataSize(memStats.Sys),       //系统内存占用
+		//HeapAlloc:          Number.ToDataSize(memStats.HeapAlloc), //堆内存分配
+		//HeapSys:            Number.ToDataSize(memStats.HeapSys),   //堆内存系统占用
+		//NumGC:              memStats.NumGC,                        //垃圾回收次数
 	}
 	return outForm
 }
